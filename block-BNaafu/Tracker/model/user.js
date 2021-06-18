77fb36878e2c7d7cda445a2eadfc14ddfb967c2c
Ayushak18 <aayushkamboj7@gmail.com>
2021-06-18 23:41:11 +0530

    Auth done

---
+++ block-BNaafu/Tracker/model/user.js
@@ -0,0 +1,37 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let bcrypt = require('bcrypt');
+
+let userSchema = new Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  age: Number,
+  phoneNumber: Number,
+  country: String,
+});
+
+userSchema.pre('save', function (next) {
+  if (this.password) {
+    bcrypt.hash(this.password, 10, (error, hashedPassword) => {
+      if (error) {
+        next(error);
+      } else {
+        this.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.verifyPassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (error, result) => {
+    return cb(error, result);
+  });
+};
+
+let User = mongoose.model('user', userSchema);
+
+module.exports = User;
---
+++ block-BNaafu/Tracker/modules/google.js
@@ -0,0 +1,15 @@
+let passport = require('passport');
+let GoogleStrategy = require('passport-google-oauth20');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+    }
+  )
+);
